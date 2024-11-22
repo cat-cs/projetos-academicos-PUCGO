ebf26623de7e5862cb4dac8b24f2cb78b672e893
Catarina Coelho <161080507+cat-cs@users.noreply.github.com>
2024-11-22 10:05:27 -0300

    create update database
    basic CRUD

--- PI B - Lista de Contatos/lista_crud.py
+++ PI B - Lista de Contatos/lista_crud.py
@@ -10,10 +10,29 @@ conexaoMysql = mysql.connector.connect(
 
 cursor = conexaoMysql.cursor()
 
-inserir = 'insert into contatos values (Lucas)'
-cursor.execute(inserir)
-conexaoMysql.commit()
-#comando = 'entre aspas simples' comando a ser executado
+
+#CREATE(Inserir no Banco de Dados)
+    #inserir = 'INSERT INTO contatos (nome) VALUE ("Joao Felipe da Silva")'
+    #cursor.execute(inserir)
+    #conexaoMysql.commit()
+
+#READ
+    #  lerBD = 'SELECT * FROM contatos'
+    #  cursor.execute(lerBD)
+    #  resultado = cursor.fetchall()
+    #  print(resultado)
+
+#UPDATE
+    # atualizaDB = 'UPDATE contatos SET nome="João Carlos da Silva Moraes" WHERE id = 1'
+    # cursor.execute(atualizaDB)
+    # conexaoMysql.commit()
+
+#DELETE
+#   deletar = 'DELETE FROM contatos WHERE id = 1' 
+#   cursor.execute(deletar)   
+#   conexaoMysql.commit()
+
+#comando = 'entre aspas simples' comando a ser executado, "asppas duplas para texto no sql"
 #cursor.execute(comando)
 
 # conexao.commit() para EDITAR banco de dados
