f3bceaa0b9a681ecef969ac15c972903dcee7c45
Catarina Coelho <161080507+cat-cs@users.noreply.github.com>
2024-11-22 11:59:50 -0300

    update

--- PI B - Lista de Contatos/lista_crud.py
+++ PI B - Lista de Contatos/lista_crud.py
@@ -10,33 +10,50 @@ conexaoMysql = mysql.connector.connect(
 
 cursor = conexaoMysql.cursor()
 
-
-#CREATE(Inserir no Banco de Dados)
-    #inserir = 'INSERT INTO contatos (nome) VALUE ("Joao Felipe da Silva")'
-    #cursor.execute(inserir)
-    #conexaoMysql.commit()
-
-#READ
-    #  lerBD = 'SELECT * FROM contatos'
-    #  cursor.execute(lerBD)
-    #  resultado = cursor.fetchall()
-    #  print(resultado)
-
-#UPDATE
-    # atualizaDB = 'UPDATE contatos SET nome="João Carlos da Silva Moraes" WHERE id = 1'
-    # cursor.execute(atualizaDB)
-    # conexaoMysql.commit()
-
-#DELETE
-#   deletar = 'DELETE FROM contatos WHERE id = 1' 
-#   cursor.execute(deletar)   
-#   conexaoMysql.commit()
-
-#comando = 'entre aspas simples' comando a ser executado, "asppas duplas para texto no sql"
+#comando = 'entre aspas simples' comando a ser executado, "aspas duplas para texto no sql"
 #cursor.execute(comando)
-
 # conexao.commit() para EDITAR banco de dados
 #resultado = cursor.fetchall() para LER banco de dados
+
+#CREATE(Inserir no Banco de Dados)
+def create (tabela, nome):
+    inserir = f'INSERT INTO contatos {tabela} VALUE ("{nome}")'
+    cursor.execute(inserir)
+    conexaoMysql.commit();
+
+#READ
+def read (tabela):
+    lerBD = f'SELECT * FROM {tabela}'
+    cursor.execute(lerBD)
+    resultado = cursor.fetchall();
+    print(resultado)
+
+#UPDATE
+def update (tabela, nome):
+    atualizaDB = f'UPDATE {tabela} SET nome="{nome}" WHERE id = 1'
+    cursor.execute(atualizaDB)
+    conexaoMysql.commit();
+
+#DELETE
+def delete (tabela, nome):
+    deletar = f'DELETE FROM {tabela} WHERE id = 1' 
+    cursor.execute(deletar)   
+    conexaoMysql.commit();
+
+opcao = input("Escolha a opção desejada \n 1- Adicionar \n 2- Ler \n 3- Atualizar \n 4- Deletar \n")
+
+if opcao == 1:
+    create()
+elif opcao == 2:
+    read()
+elif opcao == 3:
+    update()
+elif opcao == 4:
+    delete()
+else: 
+    print("Opção Inválida")
+
+
 
 #Fecha conexao
 cursor.close()
